896dcca45010501933009cc61e4f2dc85cebf053
Linus Narva <linus.narva@gmail.com>
2014-11-14 17:19:32 +0100

    Oops, forgot to add player.

---
+++ src/model/Player.hpp
@@ -0,0 +1,29 @@
+/*******************************************************
+ * Copyright (C) 2014-2015 Linus Narva
+ * linus.narva@gmail.com
+ * 
+ * This file is part of reversi-assignment.
+ * 
+ * reversi-assignment can not be copied and/or
+ * distributed without the express permission of Linus
+ * Narva.
+ *******************************************************/
+
+/*
+ * Enum class specifying a player.
+ *
+ * Linus Narva.
+ */
+#ifndef PLAYER_HPP_
+#define PLAYER_HPP_
+
+namespace reversi {
+
+enum class Player
+{
+	Empty, Black, White
+};
+
+} //namespace reversi
+
+#endif //PLAYER_HPP_
